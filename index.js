a9d6cadc97bae9fa219b994dd2b78205ff81833d
Camille Sousa <82047426+mournkid@users.noreply.github.com>
2023-08-10 15:09:57 -0300

    feito

---
+++ index.js
@@ -0,0 +1,30 @@
+const galleryElement = document.getElementById("image-gallery");
+
+async function fetchImage() {
+  imgs = "";
+
+  try {
+    await fetch(
+      `https://api.unsplash.com/search/photos?per_page=30&page=1&query=japanese-food&client_id=b1ZDS2G2jorTwN7_kiH9DPt_3c0Gkhk2Xpv-KjVaP3Y`
+    ).then((res) =>
+      res.json().then((data) => {
+        const pictures = data.results;
+        if (pictures) {
+          pictures.forEach((pic) => {
+            imgs += `
+            <li>
+              <img src=${pic.urls.small} alt="" />
+              <div class="overlay"><span>${pic.description}</span></div>
+            </li>
+            `;
+            galleryElement.innerHTML = imgs;
+          });
+        }
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchImage();
